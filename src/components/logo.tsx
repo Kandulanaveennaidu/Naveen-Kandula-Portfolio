8f3d433217353686cb29b64986b4f4eab6c3af9e
Naveen Naidu kandula <naveennaidukandula69@gmail.com>
2025-08-05 07:28:21 +0000

    Not KK Add it KN Okey

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -8,8 +8,8 @@ export default function Logo({ className }: { className?: string }) {
       xmlns="http://www.w3.org/2000/svg"
       fill="currentColor"
     >
-      <path d="M20 20 V 80 H 30 V 55 L 50 80 H 60 L 37.5 50 L 60 20 H 50 L 30 45 V 20 H 20 Z" />
-      <path d="M60 20 L 60 80 H 70 V 45 L 85 80 H 95 L 75 40 L 95 20 H 85 L 70 40 V 20 H 60 Z" />
+      <path d="M20 20 V 80 H 30 V 55 L 50 80 H 60 L 30 50 L 60 20 H 50 L 30 45 V 20 H 20 Z" />
+      <path d="M60 20 V 80 H 70 V 30 L 90 80 H 100 V 20 H 90 V 70 L 70 20 H 60 Z" />
     </svg>
   );
 }
